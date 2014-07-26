2af36eb2c1bb9b66a656841c30c5cfcd0ab083b5
Mattias Erming <mattias@mattiaserming.com>
2014-07-25 18:17:13 -0700

    Render properties

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
 
+function render(resume) {
+	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	return Handlebars.compile(template)(resume);
+}
+
 module.exports = {
-	render: function(resume) {
-		var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
-		return Handlebars.compile(template)(resume);
-	}
+	render: render
 };
