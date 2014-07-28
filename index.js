34d3433c1e2fc2e95edc849687e27e9a91d2f2a1
Mattias Erming <mattias@mattiaserming.com>
2014-07-28 03:49:42 -0700

    Include style.css

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var fs = require("fs");
 var Handlebars = require("handlebars");
 
 function render(resume) {
+	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
-	return Handlebars.compile(template)(resume);
+	return Handlebars.compile(template)({
+		css: css,
+		resume: resume
+	});
 }
 
 module.exports = {
