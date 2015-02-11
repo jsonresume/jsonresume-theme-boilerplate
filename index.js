1b5b51e21f3bfa2dff0d971bf7de4ec433816e2a
Mattias Erming <mattias@mattiaserming.com>
2015-02-11 14:53:53 -0800

    Rename template

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var Handlebars = require("handlebars");
 
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
-	return Handlebars.compile(template)({
+	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	return Handlebars.compile(tpl)({
 		css: css,
 		resume: resume
 	});
